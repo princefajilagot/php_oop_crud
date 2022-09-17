3ccee058237c998b2ba2ce51069f3e679fd03603
princefajilagot <113429391+princefajilagot@users.noreply.github.com>
2022-09-17 14:01:50 +0800

    Add validation on update

--- php_oop_crud/edit-employee.php
+++ php_oop_crud/edit-employee.php
@@ -1,13 +1,17 @@
 <?php
 require_once 'php/init.php';
-check_session();
-$errors = update();
-$emp_record = getId();
+if(isset($_POST['update'])){
+    $result = update();
+    $validate = $result[0];
+    $data = $result[1];
+}else{
+    $data = getId();
+}
 ?>
 <div class="container mt-5">
     <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
         <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a class="text-decoration-none fw-semibold" href="/php_oop_crud">Dashboard</a></li>
+            <li class="breadcrumb-item"><a class="text-decoration-none fw-semibold" onclick="backToHome()" style="cursor: pointer;">Dashboard</a></li>
             <li class="breadcrumb-item active fw-semibold" aria-current="page">Edit Employee's Record</li>
         </ol>
     </nav>
@@ -19,38 +23,53 @@ $emp_record = getId();
             <div class="card-body">
                 <div class="row">
                     <div class="col-md-6 mb-2">
-                        <input class="form-control" type="text" name="firstname" value="<?php echo htmlspecialchars($emp_record['firstname'], ENT_QUOTES); ?>" placeholder="Firstname">
-                        <div class="text-danger">
-                            <?php echo $errors['firstname'] ?? '' ?>
+                        <input class="form_data form-control <?php echo $validate['fNameInput'] ?? '' ?>" type="text" name="firstname" value="<?php echo @(htmlspecialchars($data['firstname'], ENT_QUOTES)); ?>" placeholder="Firstname">
+                        <div class="<?php echo $validate['fNameText'] ?? '' ?>">
+                            <?php echo $validate['firstname'] ?? '' ?>
                         </div>
                     </div>
                     <div class="col-md-6 mb-2">
-                        <input class="form-control" type="text" name="lastname" value="<?php echo htmlspecialchars($emp_record['lastname'], ENT_QUOTES); ?>" placeholder="Lastname">
+                        <input class="form_data form-control <?php echo $validate['lNameInput'] ?? '' ?>" type="text" name="lastname" value="<?php echo htmlspecialchars($data['lastname'], ENT_QUOTES); ?>" placeholder="Lastname">
+                        <div class="<?php echo $validate['lNameText'] ?? '' ?>">
+                            <?php echo $validate['lastname'] ?? '' ?>
+                        </div>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-md-6 mb-2">
-                        <input class="form-control" type="text" name="address" value="<?php echo htmlspecialchars($emp_record['address'], ENT_QUOTES); ?>" placeholder="Address">
+                        <input class="form_data form-control <?php echo $validate['aInput'] ?? '' ?>" type="text" name="address" value="<?php echo htmlspecialchars($data['address'], ENT_QUOTES); ?>" placeholder="Address">
+                        <div class="<?php echo $validate['addressText'] ?? '' ?>">
+                            <?php echo $validate['address'] ?? '' ?>
+                        </div>
                     </div>
                     <div class="col-md-6 mb-2">
-                        <input class="form-control" type="date" name="birthday" value="<?php echo $emp_record['birthday']; ?>" placeholder="Birthday">
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-6 mb-2">
-                        <input class="form-control" type="text" name="department" value="<?php echo htmlspecialchars($emp_record['department'], ENT_QUOTES); ?>" placeholder="Department">
-                    </div>
-                    <div class="col-md-6 mb-2">
-                        <input class="form-control" type="text" name="position" value="<?php echo htmlspecialchars($emp_record['position'], ENT_QUOTES); ?>" placeholder="Position">
+                        <input class="form_data form-control <?php echo $validate['bInput'] ?? '' ?>" type="date" name="birthday" value="<?php echo $data['birthday']; ?>" placeholder="Birthday">
+                        <div class="<?php echo $validate['bText'] ?? '' ?>">
+                            <?php echo $validate['birthday'] ?? '' ?>
+                        </div>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-md-6 mb-2">
-                        <select class="form-control" name="status" id="">
+                        <input class="form_data form-control <?php echo $validate['deptInput'] ?? '' ?>" type="text" name="department" value="<?php echo htmlspecialchars($data['department'], ENT_QUOTES); ?>" placeholder="Department">
+                        <div class="<?php echo $validate['deptText'] ?? '' ?>">
+                            <?php echo $validate['department'] ?? '' ?>
+                        </div>
+                    </div>
+                    <div class="col-md-6 mb-2">
+                        <input class="form_data form-control <?php echo $validate['posInput'] ?? '' ?>" type="text" name="position" value="<?php echo htmlspecialchars($data['position'], ENT_QUOTES); ?>" placeholder="Position">
+                        <div class="<?php echo $validate['posText'] ?? '' ?>">
+                            <?php echo $validate['position'] ?? '' ?>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6 mb-2">
+                        <select class="form_data form-control <?php echo $validate['statInput'] ?? '' ?>" name="status" id="">
                             <option value="">Choose status</option>
                             <option value="Project Based"
                                 <?php
-                                if($emp_record['status'] == "Project Based"){
+                                if($data['status'] == "Project Based"){
                                     echo "selected";
                                 }
                                 ?>>
@@ -58,7 +77,7 @@ $emp_record = getId();
                             </option>
                             <option value="Contractual"
                                 <?php
-                                    if($emp_record['status'] == "Contractual"){
+                                    if($data['status'] == "Contractual"){
                                         echo "selected";
                                     }
                                 ?>>
@@ -66,28 +85,29 @@ $emp_record = getId();
                             </option>
                             <option value="Regular"
                                 <?php
-                                    if($emp_record['status'] == "Regular"){
+                                    if($data['status'] == "Regular"){
                                         echo "selected";
                                     }
                                 ?>>
                                 Regular
                             </option>
                         </select>
+                        <div class="<?php echo $validate['statText'] ?? '' ?>">
+                            <?php echo $validate['status'] ?? '' ?>
+                        </div>
                     </div>
                     <div class="col-md-6">
-                        <input class="form-control <?php echo $errors['salaryInput'] ?? '' ?>" type="text" name="salary" value="<?php echo $emp_record['salary']; ?>" placeholder="Salary">
-                        <div class="text-danger">
-                            <?php echo $errors['salary'] ?? '' ?>
+                        <input class="form_data form-control <?php echo $validate['salaryInput'] ?? '' ?>" type="text" name="salary" value="<?php echo $data['salary']; ?>" placeholder="Salary">
+                        <div class="<?php echo $validate['salaryText'] ?? '' ?>">
+                            <?php echo $validate['salary'] ?? '' ?>
                         </div>
                     </div>
                 </div>
             </div>
             <div class="card-footer">
-                <input type="hidden" name="emp_no" value="<?php echo $emp_record['emp_no']; ?>">
-                <input type="hidden" name="id" value="<?php echo $emp_record['id']; ?>">
-                <button class="btn btn-success w-25" type="submit" name="update">Update</button>
+                <input class="form_data" type="hidden" name="id" value="<?php echo $data['id']; ?>">
+                <button class="form_data btn btn-success w-25" id="submit" type="submit" name="update">Update</button>
             </div>
         </form>
     </div>
 </div>
-<?php include('inc/footer.inc.php'); ?>
